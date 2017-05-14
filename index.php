c6162fd17d858f9a724e6325443c97c5dc5f9559
Alexei <suvorov1995@bk.ru>
2017-05-14 17:07:21 +0300

    zdraste

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'library/func.php';
+
+use library\MyZodiac as MyZodiac;
+
+try {
+    $myzodiac = new MyZodiac('1997-02-25');
+    echo $myzodiac->zodiac();
+} catch (Exception $e) {
+    echo 'Выброшено исключение: '.$e->getMessage()."\n";
+}
